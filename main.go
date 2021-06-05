c6cfea065f719d1fb769276729fb2fe8e35b1e71
mintcc342g <mintcc342g@gmail.com>
2021-06-05 14:26:01 +0900

    add. example in main

--- main.go
+++ main.go
@@ -1,1 +1,38 @@
 package main
+
+import (
+	"main/stack"
+)
+
+func main() {
+
+	stkSample := []int{3, 10, 2, 6, 1}
+	stk := stack.NewFixedStack(len(stkSample))
+
+	for _, v := range stkSample {
+		if err := stk.Push(v); err != nil {
+			print(err)
+			return
+		}
+	}
+
+	println(stk.Dump())
+	println(stk.Peek())
+
+	stk.Pop()
+	println(stk.Peek())
+
+	stk.Clear()
+
+	stkSample = []int{3, 10, 1, 6, 1}
+	for _, v := range stkSample {
+		if err := stk.Push(v); err != nil {
+			print(err)
+			return
+		}
+	}
+
+	println(stk.Count(1))
+	println(stk.Contains(10))
+	println(stk.Contains(20))
+}
