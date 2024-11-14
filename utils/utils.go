af18a20213bc5235d219336ecdfbd1da5be02ceb
mintcc342g <>
2024-11-14 09:17:11 +0900

    add a rand util func

--- utils/utils.go
+++ utils/utils.go
@@ -2,7 +2,15 @@ package utils
 
 import "math/rand"
 
-// from `1` to `n`
+// 1 ~ n
 func RandInt(n uint) int {
 	return rand.Intn(int(n)) + 1
+}
+
+// from ~ to
+func RandIntRange(from, to uint) int {
+	if from > to {
+		return int(to) + rand.Intn(int(from-to)+1)
+	}
+	return int(from) + rand.Intn(int(to-from)+1)
 }
